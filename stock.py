93d54791cf326e58ecbb4320feb177acaad07e3b
Sophie <148832181+happyface3650@users.noreply.github.com>
2024-08-14 17:35:42 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -60,21 +60,22 @@ if len(selected_stock) != 0:  # checks if characters are entered
     plot_raw_data(data)
 
     # Forecasting
-    m = fit_prophet_model(data)
-    future = m.make_future_dataframe(periods=period)
-    forecast = m.predict(future)  # predict future prices
-
-    st.subheader('Forecast data')
-    forecast_copy = forecast.copy()  # create a copy of forecast data
-    # select relevant columns for display
-    forecast_copy = forecast_copy.drop(columns=['trend_upper', 'trend_lower', 'additive_terms', 'additive_terms_lower', 'additive_terms_upper', 'weekly', 'weekly_lower', 'weekly_upper', 'yearly', 'yearly_lower', 'yearly_upper', 'multiplicative_terms', 'multiplicative_terms_lower', 'multiplicative_terms_upper'])
-    # rename columns
-    forecast_copy = forecast_copy.rename(columns={'ds': 'Date', 'yhat': 'Forecasted Prices', 'yhat_lower': 'Lower Bound', 'yhat_upper': 'Upper Bound'})
-    st.write(forecast_copy.tail())  # display data
-    fig1 = plot_plotly(m, forecast)  # combine predicted and actual prices
-    st.plotly_chart(fig1)  # plot the graph
-    st.subheader('Forecast components')
-    fig2 = m.plot_components(forecast)  # shows trend lines for different time of the day, week and year
-    st.write(fig2)
-else:  # error message if the stock ticker is wrong
-    st.error("Invalid Stock Ticker/Not enough data to make a forecast. Please try again.")
+    if df_train.dropna().shape[0] >= 2:
+        m = fit_prophet_model(data)
+        future = m.make_future_dataframe(periods=period)
+        forecast = m.predict(future)  # predict future prices
+    
+        st.subheader('Forecast data')
+        forecast_copy = forecast.copy()  # create a copy of forecast data
+        # select relevant columns for display
+        forecast_copy = forecast_copy.drop(columns=['trend_upper', 'trend_lower', 'additive_terms', 'additive_terms_lower', 'additive_terms_upper', 'weekly', 'weekly_lower', 'weekly_upper', 'yearly', 'yearly_lower', 'yearly_upper', 'multiplicative_terms', 'multiplicative_terms_lower', 'multiplicative_terms_upper'])
+        # rename columns
+        forecast_copy = forecast_copy.rename(columns={'ds': 'Date', 'yhat': 'Forecasted Prices', 'yhat_lower': 'Lower Bound', 'yhat_upper': 'Upper Bound'})
+        st.write(forecast_copy.tail())  # display data
+        fig1 = plot_plotly(m, forecast)  # combine predicted and actual prices
+        st.plotly_chart(fig1)  # plot the graph
+        st.subheader('Forecast components')
+        fig2 = m.plot_components(forecast)  # shows trend lines for different time of the day, week and year
+        st.write(fig2)
+    else:  # error message if the stock ticker is wrong
+        st.error("Invalid Stock Ticker/Not enough data to make a forecast. Please try again.")
