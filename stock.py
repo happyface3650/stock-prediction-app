4f649c4dc42bb1c9a86982d87ff9772ce98a8171
Sophie <148832181+happyface3650@users.noreply.github.com>
2025-01-20 17:07:16 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -66,7 +66,7 @@ if len(selected_stock) != 0: #checks if characters are entered
         st.subheader('Forecast data')
         forecast_copy = forecast.copy() #create a copy of forecast data
         #select relevant coumns for display
-        forecast_copy = forecast_copy.drop(columns=['trend_upper', 'trend_lower', 'additive_terms', 'additive_terms_lower', 'additive_terms_upper', 'weekly', 'weekly_lower', 'weekly_upper', 'yearly', 'yearly_lower', 'yearly_upper', 'multiplicative_terms', 'multiplicative_terms_lower', 'multiplicative_terms_upper'])
+        forecast_copy = forecast_copy.drop(columns=['trend_upper', 'trend', 'trend_lower', 'additive_terms', 'additive_terms_lower', 'additive_terms_upper', 'weekly', 'weekly_lower', 'weekly_upper', 'yearly', 'yearly_lower', 'yearly_upper', 'multiplicative_terms', 'multiplicative_terms_lower', 'multiplicative_terms_upper'])
         #rename columns 
         forecast_copy = forecast_copy.rename(columns={'ds': 'Date', 'yhat': 'Forecasted Prices', 'yhat_lower': 'Lower Bound', 'yhat_upper': 'Upper Bound'})
         predicted_close = round(forecast_copy.iloc[-1]['Forecasted Prices'], 2)
