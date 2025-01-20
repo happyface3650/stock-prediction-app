fc43151c7e4cffd7c6ab224bfdd2f95dadd78e5f
Sophie <148832181+happyface3650@users.noreply.github.com>
2025-01-20 13:15:37 -0500

    Update stock.py

--- stock.py
+++ stock.py
@@ -70,6 +70,7 @@ if len(selected_stock) != 0: #checks if characters are entered
         #rename columns 
         forecast_copy = forecast_copy.rename(columns={'ds': 'Date', 'yhat': 'Forecasted Prices', 'yhat_lower': 'Lower Bound', 'yhat_upper': 'Upper Bound'})
         st.write(forecast_copy.tail()) #display data
+        st.subheader('Prediction')
         fig1 = plot_plotly(m, forecast) #combine predicted and actual prices
         st.plotly_chart(fig1) #plot the graph
         st.subheader('Forecast components')
